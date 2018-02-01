8331af121f1c8c2620856b086d18a24c85c4713d
zakkuns <zkuns@mail.csuchico.edu>
2018-02-01 11:23:42 -0800

    A madlib about a student in professor Buffardi's class

--- madlibs.cpp
+++ madlibs.cpp
@@ -1,9 +1,29 @@
-//Authors: 
+//Authors: Zakary Kuns 
 #include <iostream>
 #include <string>
 
 int main()
 {
 
-  return 0;
+        int number;
+        std::string student;
+        double GPA;
+        char grade;
+        
+        std::cout << "Enter a name" << std::endl;
+        std::cin >> student;
+        std::cout << "Enter a number" << std::endl;
+        std::cin >> number;
+        std::cout << "Enter a decimal number 0-4" << std::endl;
+        std::cin >> GPA;
+        std::cout << "Enter a letter A-F" << std::endl;
+        std::cin >> grade;
+
+        
+        std::cout << "Mr. Buffardi had a favorite student named " << student << "." << std::endl;
+        std::cout << "He was number " << number << " in the class." << std::endl;
+        std::cout << "This was because he got a " << grade << " in Mr. Buffardi's programming class and also maintains a " << GPA << " GPA." << std::endl;
+
+
+        return 0;
 }
